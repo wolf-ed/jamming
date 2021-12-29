9f171f087d71ae9f495433681d6f0b34cd07a1be
wolf-ed <wolf7ed@gmail.com>
2021-12-29 22:50:02 +0100

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,18 @@ import SearchResults from '../SearchResults/SearchResults';
 import PlayList  from '../PlayList/PlayList';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      SearchResults: [
+        {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+      ],
+      playlistName: 'My playList'
+    }
+  }
   render() {
     return (
       < div >
@@ -13,7 +25,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
             <div className="App-playlist">
-                 <SearchResults />
+                 <SearchResults SearchResults={this.state.SearchResults}/>
                  <PlayList /> 
             </div>
         </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './TrackList.css';
+import Track from "../Track/Track";
 
 class TrackList extends React.Component {
     render() {
         return (
-            <div class="TrackList">
-                {/* <!-- You will add a map method that renders a set of Track components  --> */}
+            <div className="TrackList">
+                {
+                this.props.tracks.map(track => {
+                    return <Track track={track}
+                        key={track.id} />
+                })
+                }
             </div>
-        )
+            )
     }
 }
-
 export default TrackList;
