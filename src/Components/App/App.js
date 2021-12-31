f8964ff15967d3ee447f9731ea9d9f5934ab7831
wolf-ed <wolf7ed@gmail.com>
2021-12-31 18:45:34 +0100

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
-import PlayList  from '../PlayList/PlayList';
+import PlayList from '../PlayList/PlayList';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,29 +11,42 @@ class App extends React.Component {
 
     this.state = {
       SearchResults: [
-        {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+        { name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+        { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+        { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
       ],
       playlistName: 'My playList',
       playlistTracks: [
-        {name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id:41},
-        {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2',id: 5},
-        {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3',id: 6}
+        { name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 41 },
+        { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5 },
+        { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }
       ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      tracks.push(track)
+      this.setState({ playlistTracks: tracks })
     }
   }
+
   render() {
     return (
       < div >
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           <SearchBar />
-            <div className="App-playlist">
-                 <SearchResults SearchResults={this.state.SearchResults}/>
-                 <PlayList playlistName={this.state.playlistName}
-                           playlistTracks={this.state.playlistTracks}  /> 
-            </div>
+          <div className="App-playlist">
+            <SearchResults SearchResults={this.state.SearchResults} 
+              onAdd={this.addTrack}          />
+            <PlayList playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks} />
+          </div>
         </div>
       </div >
     )
