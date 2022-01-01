e9da04ca8a40293c409e5833f73342a692b67dbc
wolf-ed <wolf7ed@gmail.com>
2022-01-01 16:06:29 +0100

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
       ]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -35,6 +36,14 @@ class App extends React.Component {
     }
   }
 
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+
+  }
+
   render() {
     return (
       < div >
@@ -45,7 +54,8 @@ class App extends React.Component {
             <SearchResults SearchResults={this.state.SearchResults} 
               onAdd={this.addTrack}          />
             <PlayList playlistName={this.state.playlistName}
-              playlistTracks={this.state.playlistTracks} />
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack}    />
           </div>
         </div>
       </div >
